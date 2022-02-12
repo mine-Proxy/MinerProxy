5f417f64808f470c1e452d194d97705dc5724304
mine-Proxy <97606543+mine-Proxy@users.noreply.github.com>
2022-02-12 17:09:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -72,7 +72,7 @@ install_download() {
         service supervisord restart
     fi
     [ -d ./MinerProxy ] && rm -rf ./MinerProxy
-    git clone https://github.com/mine-Proxy/MinerProxy/MinerProxy.git
+    git clone https://github.com/mine-Proxy/MinerProxy.git
 
     if [[ ! -d ./MinerProxy ]]; then
         echo
