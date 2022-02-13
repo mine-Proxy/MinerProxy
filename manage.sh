88f3dd13ca5f4c787755cbe296207db9875270c5
mine-Proxy <97606543+mine-Proxy@users.noreply.github.com>
2022-02-14 00:03:44 +0800

    Add files via upload

--- manage.sh
+++ manage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-[[ $(id -u) != 0 ]] && echo -e "請在Root用戶下運行安裝該腳本" && exit 1
+[[ $(id -u) != 0 ]] && echo -e "请在Root用户下运行安装该脚本" && exit 1
 
 cmd="apt-get"
 if [[ $(command -v apt-get) || $(command -v yum) ]] && [[ $(command -v systemctl) ]]; then
@@ -7,123 +7,122 @@ if [[ $(command -v apt-get) || $(command -v yum) ]] && [[ $(command -v systemctl
         cmd="yum"
     fi
 else
-    echo "這個安裝腳本不支持你的系統" && exit 1
+    echo "这个安装脚本不支持你的系统" && exit 1
 fi
 
 
 install(){    
-    if [ -d "/root/go_miner_proxy" ]; then
-        echo -e "檢測到您已安裝GoMinerProxy，請勿重複安裝，如您確認您未安裝請使用rm -rf /root/go_miner_proxy指令" && exit 1
+    if [ -d "/root/miner_proxy" ]; then
+        echo -e "检测到您已安装MinerProxy，请勿重复安装，如您确认您未安装请使用rm -rf /root/miner_proxy指令" && exit 1
     fi
-    if screen -list | grep -q "go_miner_proxy"; then
-        echo -e "檢測到您的GoMinerProxy已啟動，請勿重複安裝" && exit 1
+    if screen -list | grep -q "miner_proxy"; then
+        echo -e "检测到您的MinerProxy已启动，请勿重复安装" && exit 1
     fi
 
     $cmd update -y
     $cmd install wget screen -y
     
-    mkdir /root/go_miner_proxy
-    wget https://raw.githubusercontent.com/GoMinerProxy/GoMinerProxy/main/scripts/run.sh -O /root/go_miner_proxy/run.sh
-    chmod 777 /root/go_miner_proxy/run.sh
-    wget https://raw.githubusercontent.com/GoMinerProxy/GoMinerProxy/main/others/cert.tar.gz -O /root/go_miner_proxy/cert.tar.gz
-    tar -zxvf /root/go_miner_proxy/cert.tar.gz -C /root/go_miner_proxy
+    mkdir /root/miner_proxy
+    wget https://raw.githubusercontent.com/mine-Proxy/MinerProxy/main/run.sh -O /root/miner_proxy/run.sh
+    chmod 777 /root/miner_proxy/run.sh
+    wget https://raw.githubusercontent.com/mine-Proxy/MinerProxy/main/server.key -O /root/miner_proxy/server.key
+    wget https://raw.githubusercontent.com/mine-Proxy/MinerProxy/main/server.pem -O /root/miner_proxy/server.pem
     
-    wget https://github.com/GoMinerProxy/GoMinerProxy/releases/download/1.3.3/GoMinerProxy_v1.3.3_linux_amd64.tar.gz -O /root/GoMinerProxy_v1.3.3_linux_amd64.tar.gz
-    tar -zxvf /root/GoMinerProxy_v1.3.3_linux_amd64.tar.gz -C /root/go_miner_proxy
-    chmod 777 /root/go_miner_proxy/GoMinerProxy
+    wget https://raw.githubusercontent.com/mine-Proxy/MinerProxy/main/MinerProxy_6.0.3_linux -O /root/miner_proxy/MinerProxy
+    chmod 777 /root/miner_proxy/MinerProxy
 
-    screen -dmS go_miner_proxy
+    screen -dmS miner_proxy
     sleep 0.2s
-    screen -r go_miner_proxy -p 0 -X stuff "cd /root/go_miner_proxy"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
-    screen -r go_miner_proxy -p 0 -X stuff "./run.sh"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
+    screen -r miner_proxy -p 0 -X stuff "cd /root/miner_proxy"
+    screen -r miner_proxy -p 0 -X stuff $'\n'
+    screen -r miner_proxy -p 0 -X stuff "./run.sh"
+    screen -r miner_proxy -p 0 -X stuff $'\n'
 
     sleep 2s
-    echo "GoMinerProxy V1.3.3已經安裝到/root/go_miner_proxy"
-    cat /root/go_miner_proxy/pwd.txt
+    echo "MinerProxy V6.3.2已经安装到/root/miner_proxy"
+    cat /root/miner_proxy/pwd.txt
     echo ""
-    echo "您可以使用指令screen -r go_miner_proxy查看程式端口和密碼"
+    echo "您可以使用指令screen -r miner_proxy查看程式端口和密码"
 }
 
 
 uninstall(){
-    read -p "您確認您是否刪除GoMinerProxy)[yes/no]：" flag
+    read -p "您确认您是否删除MinerProxy)[yes/no]：" flag
     if [ -z $flag ];then
-         echo "您未正確輸入" && exit 1
+         echo "您未正确输入" && exit 1
     else
         if [ "$flag" = "yes" -o "$flag" = "ye" -o "$flag" = "y" ];then
-            screen -X -S go_miner_proxy quit
-            rm -rf /root/go_miner_proxy
-            echo "GoMinerProxy已成功從您的伺服器上卸載"
+            screen -X -S miner_proxy quit
+            rm -rf /root/miner_proxy
+            echo "MinerProxy已成功从您的伺服器上卸载"
         fi
     fi
 }
 
 
 update(){
-    wget https://github.com/GoMinerProxy/GoMinerProxy/releases/download/1.3.3/GoMinerProxy_v1.3.3_linux_amd64.tar.gz -O /root/GoMinerProxy_v1.3.3_linux_amd64.tar.gz
+    wget https://raw.githubusercontent.com/mine-Proxy/MinerProxy/main/MinerProxy_6.0.3_linux -O /root/MinerProxy
 
-    if screen -list | grep -q "go_miner_proxy"; then
-        screen -X -S go_miner_proxy quit
+    if screen -list | grep -q "miner_proxy"; then
+        screen -X -S miner_proxy quit
     fi
-    rm -rf /root/go_miner_proxy/GoMinerProxy
+    rm -rf /root/miner_proxy/MinerProxy
 
-    tar -zxvf /root/GoMinerProxy_v1.3.3_linux_amd64.tar.gz -C /root/go_miner_proxy
-    chmod 777 /root/go_miner_proxy/GoMinerProxy
+    mv /root/MinerProxy /root/miner_proxy/MinerProxy
+    chmod 777 /root/miner_proxy/MinerProxy
 
-    screen -dmS go_miner_proxy
+    screen -dmS miner_proxy
     sleep 0.2s
-    screen -r go_miner_proxy -p 0 -X stuff "cd /root/go_miner_proxy"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
-    screen -r go_miner_proxy -p 0 -X stuff "./run.sh"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
+    screen -r miner_proxy -p 0 -X stuff "cd /root/miner_proxy"
+    screen -r miner_proxy -p 0 -X stuff $'\n'
+    screen -r miner_proxy -p 0 -X stuff "./run.sh"
+    screen -r miner_proxy -p 0 -X stuff $'\n'
 
     sleep 2s
-    echo "GoMinerProxy 已經更新至V1.3.3版本並啟動"
-    cat /root/go_miner_proxy/pwd.txt
+    echo "MinerProxy 已经更新至V6.3.2版本并启动"
+    cat /root/miner_proxy/pwd.txt
     echo ""
-    echo "您可以使用指令screen -r go_miner_proxy查看程式輸出"
+    echo "您可以使用指令screen -r miner_proxy查看程式输出"
 }
 
 
 start(){
-    if screen -list | grep -q "go_miner_proxy"; then
-        echo -e "檢測到您的GoMinerProxy已啟動，請勿重複啟動" && exit 1
+    if screen -list | grep -q "miner_proxy"; then
+        echo -e "检测到您的MinerProxy已启动，请勿重复启动" && exit 1
     fi
     
-    screen -dmS go_miner_proxy
+    screen -dmS miner_proxy
     sleep 0.2s
-    screen -r go_miner_proxy -p 0 -X stuff "cd /root/go_miner_proxy"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
-    screen -r go_miner_proxy -p 0 -X stuff "./run.sh"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
+    screen -r miner_proxy -p 0 -X stuff "cd /root/miner_proxy"
+    screen -r miner_proxy -p 0 -X stuff $'\n'
+    screen -r miner_proxy -p 0 -X stuff "./run.sh"
+    screen -r miner_proxy -p 0 -X stuff $'\n'
     
-    echo "GoMinerProxy已啟動"
-    echo "您可以使用指令screen -r go_miner_proxy查看程式輸出"
+    echo "MinerProxy已启动"
+    echo "您可以使用指令screen -r miner_proxy查看程式输出"
 }
 
 
 restart(){
-    if screen -list | grep -q "go_miner_proxy"; then
-        screen -X -S go_miner_proxy quit
+    if screen -list | grep -q "miner_proxy"; then
+        screen -X -S miner_proxy quit
     fi
     
-    screen -dmS go_miner_proxy
+    screen -dmS miner_proxy
     sleep 0.2s
-    screen -r go_miner_proxy -p 0 -X stuff "cd /root/go_miner_proxy"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
-    screen -r go_miner_proxy -p 0 -X stuff "./run.sh"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
+    screen -r miner_proxy -p 0 -X stuff "cd /root/miner_proxy"
+    screen -r miner_proxy -p 0 -X stuff $'\n'
+    screen -r miner_proxy -p 0 -X stuff "./run.sh"
+    screen -r miner_proxy -p 0 -X stuff $'\n'
 
-    echo "GoMinerProxy 已經重新啟動"
-    echo "您可以使用指令screen -r go_miner_proxy查看程式輸出"
+    echo "MinerProxy 已经重新启动"
+    echo "您可以使用指令screen -r miner_proxy查看程式输出"
 }
 
 
 stop(){
-    screen -X -S go_miner_proxy quit
-    echo "GoMinerProxy 已停止"
+    screen -X -S miner_proxy quit
+    echo "MinerProxy 已停止"
 }
 
 
@@ -135,33 +134,33 @@ change_limit(){
     fi
 
     if [[ "$change_flag" = "y" ]]; then
-        echo "系統連接數限制已修改，手動重啟下系統即可生效"
+        echo "系统连接数限制已修改，手动重启下系统即可生效"
     else
-        echo -n "您的系統連接數限制可能已修改，當前連接限制："
+        echo -n "您的系统连接数限制可能已修改，当前连接限制："
         ulimit -n
     fi
 }
 
 
 check_limit(){
-    echo -n "您的系統當前連接限制："
+    echo -n "您的系统当前连接限制："
     ulimit -n
 }
 
 
 echo "======================================================="
-echo "GoMinerProxy 一鍵腳本，脚本默认安装到/root/go_miner_proxy"
-echo "                                   腳本版本：V1.3.3"
+echo "MinerProxy 一键脚本，脚本默认安装到/root/miner_proxy"
+echo "                                   脚本版本：V6.3.2"
 echo "  1、安  装"
 echo "  2、卸  载"
 echo "  3、更  新"
 echo "  4、启  动"
 echo "  5、重  启"
 echo "  6、停  止"
-echo "  7、一鍵解除Linux連接數限制(需手動重啟系統生效)"
-echo "  8、查看當前系統連接數限制"
+echo "  7、一键解除Linux连接数限制(需手动重启系统生效)"
+echo "  8、查看当前系统连接数限制"
 echo "======================================================="
-read -p "$(echo -e "請選擇[1-8]：")" choose
+read -p "$(echo -e "请选择[1-8]：")" choose
 case $choose in
     1)
         install
@@ -188,6 +187,6 @@ case $choose in
         check_limit
         ;;
     *)
-        echo "請輸入正確的數字！"
+        echo "请输入正确的数字！"
         ;;
 esac
